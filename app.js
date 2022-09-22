a92f3ddcad1d34395c1ee64aa5c81c89efcb87dd
JoySystem_v <fvv2011@gmail.com>
2022-09-22 16:25:11 +0300

    drag drop finished

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
 
 
 
 item.addEventListener('dragstart', dragstart);
 item.addEventListener('dragend', dragend);
+
+
+for (const placeholder of placeholders) {
+
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragDrop);
+}
 
 function dragstart(event){
 
@@ -21,4 +32,33 @@ function dragend (event) {
 
     
     event.target.className = "item";
+}
+
+
+function dragover (event) {
+
+    event.preventDefault();
+   
+   
+}
+
+function dragenter (event) {
+
+ 
+    event.target.classList.add('hovered');
+   
+}
+
+function dragleave (event) {
+   
+    event.target.classList.remove('hovered');
+   
+}
+
+function dragDrop (event) {
+    
+    event.target.classList.remove('hovered');
+    event.target.append(item);
+   
+   
 }
